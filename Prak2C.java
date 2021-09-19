f61f698a7229db8cfdeb5b8cea4ad3cf94c6e50e
ahmadf <ahmadfajrii227@gmail.com>
2021-09-19 13:59:38 +0700

    Prak PBO

---
+++ Prak2C.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Prak2C {
+    public static void main (String[] args){
+        String BurungBeo;
+        Scanner Masukan = new Scanner(System.in);
+        BurungBeo = Masukan.nextLine();
+
+        System.out.println(BurungBeo);
+    }
+    
+}
